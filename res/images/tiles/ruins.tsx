d0fe6078fa0c3aa7789956e6aa8b59d1cde94821
FireLestics <mihailbasharin01@mail.ru>
2025-07-19 00:53:03 +0800

    Auto commit Sat 07/19/2025  0:53:03.29

---
+++ res/images/tiles/ruins.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ruins" tilewidth="20" tileheight="20" tilecount="231" columns="11">
+ <image source="ruins.png" width="220" height="420"/>
+</tileset>
